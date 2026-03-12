063ee4d930bd3af25ebdd8af08f4c51de03e5bf7
Mansoor Shah <ammansoor007@gmail.com>
2026-03-12 12:25:10 +0500

    temporarily disable QuickQuote

--- app/ClientLayout.js
+++ app/ClientLayout.js
@@ -1,14 +1,15 @@
+// app/ClientLayout.js
 'use client';
 
 import Navbar from './components/Navbar';
 import Footer from './components/Footer';
-import QuickQuote from './components/QuickQuote';
+// import QuickQuote from './components/QuickQuote'; // COMMENT THIS OUT
 
 export default function ClientLayout({ children }) {
     return (
         <>
             <Navbar />
-            <QuickQuote />
+            {/* <QuickQuote /> */} {/* COMMENT THIS OUT */}
             <main className="min-h-screen">
                 {children}
             </main>
